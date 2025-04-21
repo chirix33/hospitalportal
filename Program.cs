d29e3109ecd1985e610c152fb365696aa5e9cfa1
Ashraf Abdul-Muumin <chirixteam@gmail.com>
2025-04-21 17:10:30 -0500

    Make environment development friendly for testing

--- Program.cs
+++ Program.cs
@@ -4,18 +4,19 @@ using Sustainsys.Saml2.AspNetCore2;
 using Sustainsys.Saml2.Metadata;
 
 var builder = WebApplication.CreateBuilder(args);
+var samlSection = builder.Configuration.GetSection("Saml");
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = Saml2Defaults.Scheme; // This is key!
+    options.DefaultChallengeScheme = Saml2Defaults.Scheme;
 })
 .AddCookie()
 .AddSaml2(options =>
 {
-    options.SPOptions.EntityId = new EntityId("https://localhost:5000/Saml/Metadata");
+    options.SPOptions.EntityId = new EntityId(samlSection["EntityId"]);
 
     options.IdentityProviders.Add(
         new IdentityProvider(
@@ -23,7 +24,7 @@ builder.Services.AddAuthentication(options =>
             options.SPOptions)
         {
             LoadMetadata = true,
-            MetadataLocation = "https://dev-51454621.okta.com/app/exkodeqq561QrJsD95d7/sso/saml/metadata"
+            MetadataLocation = samlSection["Metadata"]
         });
 
     options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
@@ -37,17 +38,16 @@ var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+    app.UseHttpsRedirection();
 }
 
 app.Urls.Add(app.Environment.IsDevelopment() ? $"https://localhost:{port}" : $"http://*:{port}");
 app.Urls.Add("https://localhost:5001");
 app.UseRouting();
-app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapStaticAssets();
 app.MapControllers();
 app.MapRazorPages()
    .WithStaticAssets();
-
 app.Run();
