4ba0828a4d82e3e962618a3f6660c8ccaa74f577
Ashraf Abdul-Muumin <chirixteam@gmail.com>
2025-04-21 00:32:46 -0500

    Add Admin route

---
+++ Pages/Admin/Doctors.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HospitalPortal.Pages.Admin
+{
+    public class DoctorsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Admin/Patients.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HospitalPortal.Pages.Admin
+{
+    public class PatientsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
